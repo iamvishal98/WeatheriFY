de44592c7250056d692e67a566e68c63289f8e07
Vishal Chauhan <vishalchauhan@Vishals-MacBook-Air.local>
2023-01-24 15:15:26 +0530

    UI CHANGE , DYNAMIC BG , UNIT FILTER

--- src/App.js
+++ src/App.js
@@ -1,74 +1,95 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
 
   const [data,setData] = useState({});
-  const [location,setLocation] = useState('');
+  const [unit , setUnit] = useState('metric');
+  const [city ,setCity] = useState('');
 
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=82a470c3e3978a6172106e2bb362926e`;
 
-  const searchLocation = (e) => {
-    if(e.key === 'Enter'){
-      axios.get(URL).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      })
-      setLocation('')
+  const fetchWeatherData = async (location,measure) => {
+    try{
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${measure}&appid=82a470c3e3978a6172106e2bb362926e`;
+    const result = await axios.get(url);
+    console.log(result.data);
+    setData(result.data);
+    }catch(err) {
+      alert('city not found !!')
     }
   }
 
+
+  useEffect(()=> {
+    if(!city) return;
+    fetchWeatherData(city,unit);
+  },[city,unit])
+
+
+
+  const weatherUnit = async () => {
+    if (unit === 'metric') setUnit('imperial');  
+    else setUnit('metric');
+  }
+
+  const enterKey = (e) => {
+    if(e.key === 'Enter') {
+      setCity(e.target.value);
+      e.target.value = '';
+    }
+  }
+
+
   return (
     
-    <div className="App">
-      <div className="search">
-        <input 
-        value={location}
-        onChange={(e) => {setLocation(e.target.value)}}
-        onKeyDown={searchLocation}
-        placeholder="Enter Location"
-        type="text" />
+    <div className=
+        {
+        (data.weather) ?
+        ( (data.weather[0].main === 'Haze' || data.weather[0].main === 'Mist') ? "App haze" : 
+            ((data.weather[0].main === 'Snow') ? "App snow" : 
+              ((data.weather[0].main === 'Rain') ? "App rain" : "App"))) 
+        :"App"
+        }
+    >
+      <div className="overlay">
+        <div className="wrapper">
+          <div className="header">
+            <input onKeyDown={enterKey} type='text' placeholder="Type City here.."/>
+            <button onClick={weatherUnit}>{unit === 'metric' ? '°C' : '°F'}</button>
+          </div>
+          <div className="container">
+            <div className="top">
+              <div className="details">
+                <div className="temperature">
+                  {data.main ? <h1>{data.main.temp.toFixed()}{unit === 'metric' ? '°C' : '°F'}</h1> : '--'}
+                </div>
+                <div className="cityname">
+                  {data.name ? <p>{data.name}</p> : '---'}
+                </div>
+              </div>
+              <div className="description">
+                {data.weather? <p>{data.weather[0].main}</p> : '---'}
+              </div>
+            </div>
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p>{data.main.feels_like.toFixed()}{unit === 'metric' ? '°C' : '°F'}</p> : '--'}
+                <span>Feels</span>
+              </div>
+              <div className="humidity">
+                {data.main ? <p>{data.main.humidity}%</p> : '--'}
+                <span>Humidity</span>
+              </div>
+              <div className="windspeed">
+                {data.wind ? <p>{data.wind.speed}</p> : '--'}
+                <span>MPH</span>
+              </div>
+            </div>
+          </div>
+        
+        </div>
       </div>
 
-      {data.name !=undefined &&
-        <div className="container">
-
-        <div className="top">
-
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-
-          <div className="temperature">
-            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
-          </div>
-
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
-
-        </div>
-
-        <div className="bottom">
-
-          <div className="feels">
-            {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> : '--'}
-            <p>FEELS LIKE</p>
-          </div>
-
-          <div className="humidity">
-            {data.main ? <p className="bold">{data.main.humidity}%</p> : '--'}
-            <p>HUMIDITY</p>
-          </div>
-
-          <div className="winds">
-            {data.wind ? <p className="bold">{data.wind.speed}</p> : '--'}
-            <p>WIND[MPH]</p>
-          </div>
-
-        </div>
-
-      </div> }
     </div>
   );
 }
